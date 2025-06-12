eec65aaad7ab05f7a6103c74b20a6b4e30b08a54
CristobalFloresH <cristobal.flores.h@mail.pucv.cl>
2025-06-12 12:58:39 -0400

    Update and rename Agregar_Causa to Agregar_Causa.c

--- Funciones_Agregar/Agregar_Causa.c
+++ Funciones_Agregar/Agregar_Causa.c
@@ -5,26 +5,26 @@ void agregarCausa(struct nodoCausas **listaCausas, struct causa *nuevaCausa){
         printf("Error al asignar memoria");
         return;
     }
-    
+
     //Se le asigna al nuevo nodo la nueva causa
     nuevoNodo->datosCausa = nuevaCausa;
-    
-    
+
+
     if(*listaCausas == NULL){
         //Lista vacia, se agrega al inicio
         nuevoNodo->siguiente = nuevoNodo;
         nuevoNodo->anterior = nuevoNodo;
         *listaCausas = nuevoNodo;
-        
+
     } else {
         //Si la lista no esta vacia se reenlazan los nodos
         struct nodoCausas *ultimo = (*listaCausas)->anterior;
-        
+
         nuevoNodo->siguiente = *listaCausas;
         nuevoNodo->anterior = ultimo;
-        
+
         ultimo->siguiente = nuevoNodo;
-        
+
         //Se actualiza la lista ya que se paso por referencia
         (*listaCausas)->anterior = nuevoNodo;
     }
