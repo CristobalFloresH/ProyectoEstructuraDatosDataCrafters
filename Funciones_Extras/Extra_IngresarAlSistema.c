0fea779ad5d035d1789dd263c9c6912015957065
CristobalFloresH <cristobal.flores.h@mail.pucv.cl>
2025-06-12 13:18:11 -0400

    Create Extra_IngresarAlSistema.c

---
+++ Funciones_Extras/Extra_IngresarAlSistema.c
@@ -0,0 +1,62 @@
+void ingresarAlSistema (struct ministerio *ministerio) {
+    char palabra[100];
+    int rol;
+
+    struct persona *nuevaPersona = (struct persona *)malloc(sizeof(struct persona));
+
+    printf("\n=========== Bienvenido al sistema ===========\n");
+    printf("Ingrese su RUT.\n\n");
+    printf("Ingrese opcion:");
+    scanf("%s", palabra);
+    getchar();
+
+    nuevaPersona->rut = (char *)malloc(strlen(palabra) + 1);
+    strcpy(nuevaPersona->rut, palabra);
+    printf("Ingrese su nombre.\n\n");
+    printf("Ingrese opcion:");
+    scanf("%s", palabra);
+    getchar();
+
+    nuevaPersona->nombre = (char *)malloc(strlen(palabra) + 1);
+    strcpy(nuevaPersona->nombre, palabra);
+    printf("Ingrese su apellido.\n\n");
+    printf("Ingrese opcion:");
+    scanf("%s", palabra);
+    getchar();
+
+    nuevaPersona->apellido = (char *)malloc(strlen(palabra) + 1);
+    strcpy(nuevaPersona->apellido, palabra);
+    printf("Ingrese su rol (1 = Usuario, 2 = Fiscal, 3 = Juez).\n\n");
+    printf("Ingrese opcion:");
+    scanf("%d", &rol);
+    getchar();
+
+    if (rol == 1) {
+        nuevaPersona->rol = rol;
+        nuevaPersona->contrasena = NULL;
+    }
+    else if (rol == 2 || rol == 3){
+        printf("Ingrese contrasena del sistema.\n\n");
+        printf("Ingrese opcion:");
+        scanf("%s", palabra);
+        getchar();
+
+        if (strcmp(palabra, CONTRASENAFISCALJUEZ) == 0) {
+            nuevaPersona->rol = rol;
+            nuevaPersona->contrasena = (char *)malloc(strlen(palabra) + 1);
+            strcpy(nuevaPersona->contrasena, palabra);
+        }
+        else {
+            printf("Contrasena incorrecta ingresando como usuario.\n\n");
+            nuevaPersona->rol = 1;
+            nuevaPersona->contrasena = NULL;
+        }
+    }
+    else{
+        printf("Opcion no valida. Intente de nuevo.\n");
+        return;
+    }
+    nuevaPersona->denuncias = NULL;
+    agregarPersonas(&(ministerio), nuevaPersona);
+    printf("Ingresando al menu...\n\n");
+}
