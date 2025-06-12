05f822c3788906cb4ba073c44ba693d386fe2d92
CristobalFloresH <cristobal.flores.h@mail.pucv.cl>
2025-06-12 13:06:34 -0400

    Update and rename Buscar_Menu to Buscar_Menu.c

--- Funciones_Buscar/Buscar_Menu.c
+++ Funciones_Buscar/Buscar_Menu.c
@@ -10,14 +10,14 @@ void menuBuscar(struct ministerio *ministerio) {
     struct involucrados *involucradoEncontrado;
 
     while (opcion != 0) {
-        printf("\n--- MENÚ BUSCAR ---\n");
+        printf("\n=========== MENU BUSCAR ===========\n");
         printf("1. Buscar persona por RUT\n");
         printf("2. Buscar denuncia por RUC (en denuncias de una persona)\n");
         printf("3. Buscar causa por RUC\n");
         printf("4. Buscar involucrado por RUT (en una causa)\n");
         printf("5. Buscar datos de carpeta por tipo (en una causa)\n");
-        printf("0. Volver\n");
-        printf("Seleccione una opción: ");
+        printf("0. Volver.\n");
+        printf("Seleccione una opcion: ");
         scanf("%d", &opcion);
 
         switch (opcion) {
@@ -28,7 +28,7 @@ void menuBuscar(struct ministerio *ministerio) {
                 if (personaEncontrada != NULL)
                     printf("Persona encontrada: %s %s\n", personaEncontrada->nombre, personaEncontrada->apellido);
                 else
-                    printf("No se encontró persona con ese RUT.\n");
+                    printf("No se encontro persona con ese RUT.\n");
                 break;
 
             case 2: // Denuncia por RUC en denuncias de persona
@@ -36,7 +36,7 @@ void menuBuscar(struct ministerio *ministerio) {
                 scanf("%s", rut);
                 personaEncontrada = buscarPersonaPorRut(ministerio->personas, rut);
                 if (personaEncontrada == NULL || personaEncontrada->denuncias == NULL) {
-                    printf("No se encontró la persona o no tiene denuncias.\n");
+                    printf("No se encontro la persona o no tiene denuncias.\n");
                     break;
                 }
                 printf("Ingrese RUC de la denuncia: ");
@@ -45,7 +45,7 @@ void menuBuscar(struct ministerio *ministerio) {
                 if (denunciaEncontrada != NULL)
                     printf("Denuncia encontrada: %s\n", denunciaEncontrada->descripcion);
                 else
-                    printf("No se encontró la denuncia con ese RUC para esa persona.\n");
+                    printf("No se encontro la denuncia con ese RUC para esa persona.\n");
                 break;
 
             case 3: // Causa por RUC
@@ -55,7 +55,7 @@ void menuBuscar(struct ministerio *ministerio) {
                 if (causaActual != NULL)
                     printf("Causa encontrada: %s\n", causaActual->ruc);
                 else
-                    printf("No se encontró la causa con ese RUC.\n");
+                    printf("No se encontro la causa con ese RUC.\n");
                 break;
 
             case 4: // Involucrado por RUT (en una causa)
@@ -63,7 +63,7 @@ void menuBuscar(struct ministerio *ministerio) {
                 scanf("%s", ruc);
                 causaActual = buscarCausaPorRuc(ministerio->causas, ruc);
                 if (causaActual == NULL || causaActual->involucrados == NULL) {
-                    printf("No se encontró la causa o no tiene involucrados.\n");
+                    printf("No se encontro la causa o no tiene involucrados.\n");
                     break;
                 }
                 printf("Ingrese RUT del involucrado: ");
@@ -72,7 +72,7 @@ void menuBuscar(struct ministerio *ministerio) {
                 if (involucradoEncontrado != NULL)
                     printf("Involucrado encontrado: %s %s\n", involucradoEncontrado->persona->nombre, involucradoEncontrado->persona->apellido);
                 else
-                    printf("No se encontró involucrado con ese RUT en esa causa.\n");
+                    printf("No se encontro involucrado con ese RUT en esa causa.\n");
                 break;
 
             case 5: // Buscar datos carpeta por tipo en una causa
@@ -80,10 +80,10 @@ void menuBuscar(struct ministerio *ministerio) {
                 scanf("%s", ruc);
                 causaActual = buscarCausaPorRuc(ministerio->causas, ruc);
                 if (causaActual == NULL || causaActual->datosCarpetas == NULL) {
-                    printf("No se encontró la causa o no tiene datos de carpeta.\n");
+                    printf("No se encontro la causa o no tiene datos de carpeta.\n");
                     break;
                 }
-                printf("Ingrese tipo de dato (1=Declaración, 2=Prueba, 3=Diligencia, 4=Denuncia): ");
+                printf("Ingrese tipo de dato (1=Declaracion, 2=Prueba, 3=Diligencia, 4=Denuncia): ");
                 scanf("%d", &tipoDato);
                 listaResultados = buscarDatosCarpetaPorTipo(causaActual, tipoDato);
                 if (listaResultados == NULL)
@@ -93,11 +93,11 @@ void menuBuscar(struct ministerio *ministerio) {
                 break;
 
             case 0:
-                printf("Saliendo del menú buscar.\n");
+                //volvemos al menu anterior
                 break;
 
             default:
-                printf("Opción no válida. Intente de nuevo.\n");
+                printf("Opcion no valida. Intente de nuevo.\n");
                 break;
         }
     }
