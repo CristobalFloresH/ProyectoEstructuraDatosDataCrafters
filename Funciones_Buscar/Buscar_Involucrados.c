c4900785e428cf02c77fb3762512fc2ebffd7115
CristobalFloresH <cristobal.flores.h@mail.pucv.cl>
2025-06-12 13:05:14 -0400

    Update and rename Buscar_Involucrados to Buscar_Involucrados.c

--- Funciones_Buscar/Buscar_Involucrados.c
+++ Funciones_Buscar/Buscar_Involucrados.c
@@ -1,6 +1,7 @@
-struct involucrados* buscarInvolucradoPorRut(struct involucrados **involucrados, int tam, const char *rut) {
+struct involucrados* buscarInvolucradoPorRut(struct involucrados **involucrados, int tam, char *rut) {
     if (involucrados == NULL) return NULL;
-    for (int i = 0; i < tam; i++) {
+    int i;
+    for (i = 0; i < tam; i++) {
         if (involucrados[i] != NULL && involucrados[i]->persona != NULL) {
             if (strcmp(involucrados[i]->persona->rut, rut) == 0) {
                 return involucrados[i];
