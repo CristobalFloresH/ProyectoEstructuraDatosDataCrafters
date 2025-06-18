dc1e0e02a12bce79192bdce920828d9abb571da1
Bushhz <rafael.valdessilva@gmail.com>
2025-06-18 18:40:26 -0400

    Update Eliminar_Personas.c

--- Funciones_Eliminar/Eliminar_Personas.c
+++ Funciones_Eliminar/Eliminar_Personas.c
@@ -1,45 +1,45 @@
-struct nodoPersonasABB* minimo(struct nodoPersonasABB *arbol) { // esta funcion busca el rut mas bajo, es decir el nodo mas a la izquierda en el arbol.
+struct nodoPersonasABB* minimo(struct nodoPersonasABB *arbol) {
     while (arbol != NULL && arbol->izquierda != NULL)
-        arbol = arbol->izquierda; // simplemente nos movemos a la izquierda hasta encontrar el menor.
+        arbol = arbol->izquierda;
     return arbol;
 }
-
 void eliminarPersona(struct nodoPersonasABB **personas, char *rut) {
+    int comparar;
+    struct nodoPersonasABB *nodoEliminar;
+    struct nodoPersonasABB *minNodo;
+    struct persona *aux;
+
     if (personas == NULL || *personas == NULL) {
         printf("No se encontraron personas\n");
         return;
     }
 
-    int comparar = strcmp(rut, (*personas)->datosPersona->rut);
+    comparar = strcmp(rut, (*personas)->datosPersona->rut);
 
     if (comparar < 0) {
         eliminarPersona(&((*personas)->izquierda), rut);
-    }
-    else if (comparar > 0) {
+    } else if (comparar > 0) {
         eliminarPersona(&((*personas)->derecha), rut);
-    }
-    else {
-        struct nodoPersonasABB *nodoEliminar = *personas;
+    } else {
+        nodoEliminar = *personas;
 
         if (nodoEliminar->izquierda == NULL) {
             *personas = nodoEliminar->derecha;
             liberarPersona(nodoEliminar->datosPersona);
             free(nodoEliminar);
-        }
-        else if (nodoEliminar->derecha == NULL) {
+        } else if (nodoEliminar->derecha == NULL) {
             *personas = nodoEliminar->izquierda;
             liberarPersona(nodoEliminar->datosPersona);
             free(nodoEliminar);
-        }
-        else {
-            struct nodoPersonasABB *minNodo = minimo(nodoEliminar->derecha);
-
-            struct persona *aux = nodoEliminar->datosPersona;
+        } else {
+            minNodo = minimo(nodoEliminar->derecha);
+            aux = nodoEliminar->datosPersona;
             nodoEliminar->datosPersona = minNodo->datosPersona;
             minNodo->datosPersona = aux;
 
             eliminarPersona(&(nodoEliminar->derecha), aux->rut);
         }
     }
+
     printf("Persona eliminada correctamente.");
 }
