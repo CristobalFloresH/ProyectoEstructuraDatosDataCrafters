3775d306936f85842c53ff38702783d30fe64886
DeTeX360 <35928513+DeTeX360@users.noreply.github.com>
2025-06-18 18:08:37 -0400

    Update Agregar_Causa.c

--- Funciones_Agregar/Agregar_Causa.c
+++ Funciones_Agregar/Agregar_Causa.c
@@ -1,31 +1,30 @@
 void agregarCausa(struct nodoCausas **listaCausas, struct causa *nuevaCausa){
-    //Se crea el nuevo Nodo al que se le asignara la nueva causa
-    struct nodoCausas *nuevoNodo = (struct nodoCausas *)malloc(sizeof(struct nodoCausas));
+    struct nodoCausas *nuevoNodo;
+    struct nodoCausas *ultimo;
+
+    // Se crea el nuevo Nodo al que se le asignará la nueva causa
+    nuevoNodo = (struct nodoCausas *)malloc(sizeof(struct nodoCausas));
     if(nuevoNodo == NULL){
         printf("Error al asignar memoria");
         return;
     }
 
-    //Se le asigna al nuevo nodo la nueva causa
+    // Se le asigna al nuevo nodo la nueva causa
     nuevoNodo->datosCausa = nuevaCausa;
 
-
     if(*listaCausas == NULL){
-        //Lista vacia, se agrega al inicio
+        // Lista vacía, se agrega al inicio
         nuevoNodo->siguiente = nuevoNodo;
         nuevoNodo->anterior = nuevoNodo;
         *listaCausas = nuevoNodo;
-
     } else {
-        //Si la lista no esta vacia se reenlazan los nodos
-        struct nodoCausas *ultimo = (*listaCausas)->anterior;
+        // Si la lista no está vacía se reenlazan los nodos
+        ultimo = (*listaCausas)->anterior;
 
         nuevoNodo->siguiente = *listaCausas;
         nuevoNodo->anterior = ultimo;
 
         ultimo->siguiente = nuevoNodo;
-
-        //Se actualiza la lista ya que se paso por referencia
         (*listaCausas)->anterior = nuevoNodo;
     }
 }
