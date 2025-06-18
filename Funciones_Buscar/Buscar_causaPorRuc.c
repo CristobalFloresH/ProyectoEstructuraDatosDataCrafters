fe056f1641412d3224b96a7ddf78afbff4174574
DeTeX360 <35928513+DeTeX360@users.noreply.github.com>
2025-06-18 18:55:39 -0400

    Update Buscar_causaPorRuc.c

--- Funciones_Buscar/Buscar_causaPorRuc.c
+++ Funciones_Buscar/Buscar_causaPorRuc.c
@@ -1,9 +1,9 @@
 struct causa *buscarCausaPorRuc(struct nodoCausas *causas, char *rucBuscado) {
-
+    struct nodoCausas *actual;
     //si la lista de causas o el ruc buscado es null, retorna null ya que no hay nada que buscar
     if (causas == NULL || rucBuscado == NULL) return NULL;
 
-    struct nodoCausas *actual = causas;
+    actual = causas;
 
     //se recorre lista circular hasta encontrar el ruc igual, si no se encuentra retorna null
     do {
