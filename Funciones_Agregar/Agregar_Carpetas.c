5d612ea7acaf9331b1b59c73d6a39ac3c170efe3
DeTeX360 <35928513+DeTeX360@users.noreply.github.com>
2025-06-18 18:08:03 -0400

    Update Agregar_Carpetas.c

--- Funciones_Agregar/Agregar_Carpetas.c
+++ Funciones_Agregar/Agregar_Carpetas.c
@@ -1,11 +1,15 @@
 void agregarCarpetaInvestigativa(struct nodoDatosCarpetas **carpetas, struct datosCarpeta *nuevaCarpeta) {
+    struct nodoDatosCarpetas *nuevoNodo;
+    struct nodoDatosCarpetas *actual;
+
     // Revisamos que la nueva carpeta exista
     if(nuevaCarpeta == NULL){
         printf("Nueva carpeta no existe\n");
         return;
     }
+
     // Definimos nuevo nodo para asignarle nuevaCarpeta y corroboramos que se asigne memoria
-    struct nodoDatosCarpetas *nuevoNodo = (struct nodoDatosCarpetas *)malloc(sizeof(struct nodoDatosCarpetas));
+    nuevoNodo = (struct nodoDatosCarpetas *)malloc(sizeof(struct nodoDatosCarpetas));
     if(nuevoNodo == NULL){
         printf("Error al asignar memoria para el nuevo nodo\n");
         return;
@@ -16,18 +20,17 @@ void agregarCarpetaInvestigativa(struct nodoDatosCarpetas **carpetas, struct dat
     nuevoNodo->siguiente = NULL;
     nuevoNodo->anterior = NULL;
 
-    // Si esta vacia asignar al inicio
+    // Si está vacía, asignar al inicio
     if(*carpetas == NULL){
         *carpetas = nuevoNodo;
     } else {
-
-        // Recorre struct datos carpetas hasta el final de la lista
-        struct nodoDatosCarpetas *actual = *carpetas;
+        // Recorre hasta el final de la lista
+        actual = *carpetas;
         while(actual->siguiente != NULL) {
             actual = actual->siguiente;
         }
 
-        // al encontrar final, se asignan los punteros al nuevo nodo
+        // Asigna los punteros al nuevo nodo
         actual->siguiente = nuevoNodo;
         nuevoNodo->anterior = actual;
     }
