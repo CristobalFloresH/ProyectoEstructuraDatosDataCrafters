2ad996fdeea48984b27def99d52b99e9feed9432
DeTeX360 <35928513+DeTeX360@users.noreply.github.com>
2025-06-18 18:50:52 -0400

    Update Buscar_Denuncia_Por_Ruc.c

--- Funciones_Buscar/Buscar_Denuncia_Por_Ruc.c
+++ Funciones_Buscar/Buscar_Denuncia_Por_Ruc.c
@@ -1,9 +1,11 @@
 struct denuncia *buscarDenunciaPorRuc(struct nodoDenuncias *listaDenuncias, char *rucBuscado){
-    struct nodoDenuncias *actual = listaDenuncias;
+    struct nodoDenuncias *actual;
 
     //se recorre la lista mientras queden nodos por revisar
     //y pregunta si el ruc actual es igual al buscado
 
+    actual = listaDenuncias;
+    
     while(actual != NULL){
 
         if(strcmp(actual->datosDenuncia->ruc, rucBuscado) == 0){
