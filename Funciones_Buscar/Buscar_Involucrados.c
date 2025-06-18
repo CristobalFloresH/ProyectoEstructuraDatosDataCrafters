523a4975d25513a3ff020a648b7a77f90bba85fb
DeTeX360 <35928513+DeTeX360@users.noreply.github.com>
2025-06-18 18:53:34 -0400

    Update Buscar_Involucrados.c

--- Funciones_Buscar/Buscar_Involucrados.c
+++ Funciones_Buscar/Buscar_Involucrados.c
@@ -1,6 +1,8 @@
 struct involucrados* buscarInvolucradoPorRut(struct involucrados **involucrados, int tam, char *rut) {
-    if (involucrados == NULL) return NULL;
     int i;
+    
+    if (involucrados == NULL) return NULL;
+    
     for (i = 0; i < tam; i++) {
         if (involucrados[i] != NULL && involucrados[i]->persona != NULL) {
             if (strcmp(involucrados[i]->persona->rut, rut) == 0) {
