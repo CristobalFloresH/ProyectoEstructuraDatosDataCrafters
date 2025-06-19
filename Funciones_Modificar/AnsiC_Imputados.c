1bb7477191f7271eaabb47a7f745082f1f5162b4
MauriCrafter <maurix.koko@gmail.com>
2025-06-18 20:44:10 -0400

    Update AnsiC_Imputados.c

--- Funciones_Modificar/AnsiC_Imputados.c
+++ Funciones_Modificar/AnsiC_Imputados.c
@@ -1,15 +1,24 @@
 void modificarImputado(struct ministerio *ministerio, char *rucCausa, char *rutImputado) {
+    struct causa *c = NULL;
+    struct involucrados *imp = NULL;
+    struct datosImputados *datos = NULL;
+    int opcion = -1;
+    char palabras[300];
+    char *nuevaCadena = NULL;
+    int nueva_medida = 0;
+    int nuevo_estado = 0;
+
     if (ministerio == NULL || rucCausa == NULL || rutImputado == NULL) {
         return;
     }
 
-    struct causa *c = buscarCausaPorRuc(ministerio->causas, rucCausa);
+    c = buscarCausaPorRuc(ministerio->causas, rucCausa);
     if (c == NULL) {
         printf("Causa con RUC %s no encontrada.\n", rucCausa);
         return;
     }
 
-    struct involucrados *imp = buscarImputadoEnCausa(c, rutImputado);
+    imp = buscarImputadoEnCausa(c, rutImputado);
     if (imp == NULL || imp->tipoInvolucrado != 2) {
         printf("Imputado con RUT %s no encontrado en la causa.\n", rutImputado);
         return;
@@ -25,11 +34,7 @@ void modificarImputado(struct ministerio *ministerio, char *rucCausa, char *rutI
         }
     }
 
-    struct datosImputados *datos = imp->datosImputados;
-
-    int opcion = -1;
-    char palabras[300];
-    char *nuevaCadena;
+    datos = imp->datosImputados;
 
     while (opcion != 0) {
         printf("\n--- Modificar Imputado ---\n");
@@ -59,24 +64,19 @@ void modificarImputado(struct ministerio *ministerio, char *rucCausa, char *rutI
                 break;
 
             case 2:
-                int nueva_medida;
                 do {
                     printf("Ingrese nueva medida cautelar (1 = Prision Preventiva, 2 = Arraigo Nacional, 3 = Firma Periodica, "
                            "4 = Orden de Alejamiento, 5 = Arresto Domiciliario, 6 = Libertad bajo Fianza, 7 = Ninguna): ");
-
                     scanf("%d", &nueva_medida);
                     getchar();
-
-
                     if (nueva_medida < 1 || nueva_medida > 7) {
                         printf("Medida cautelar no valida. Intente nuevamente.\n");
-                        }
-
+                    }
                 } while (nueva_medida < 1 || nueva_medida > 7);
 
                 datos->medidasCautelar = nueva_medida;
                 printf("Medida cautelar actualizada correctamente.\n");
-
+                break;
 
             case 3:
                 printf("Ingrese nueva fecha de inicio (YYYY-MM-DD): ");
@@ -107,29 +107,7 @@ void modificarImputado(struct ministerio *ministerio, char *rucCausa, char *rutI
                 break;
 
             case 5:
-                int nuevo_estado;
                 do {
                     printf("Ingrese nuevo estado procesal (1: Cautelar, 2: Formalizado, 3: Sobreseido): ");
+                    sca
 
-                    scanf("%d", &nuevo_estado);
-                    getchar();
-
-                    if (nuevo_estado < 1 || nuevo_estado > 3) {
-                    printf("Estado procesal no valido. Intente nuevamente.\n");
-                    }
-
-                } while (nuevo_estado < 1 || nuevo_estado > 3);
-
-                datos->estadoProcesal = nuevo_estado;
-                printf("Estado procesal actualizado correctamente.\n");
-
-            case 0:
-                //Volver.
-                break;
-
-            default:
-                printf("Opcion no valida.\n");
-                break;
-        }
-    }
-}
