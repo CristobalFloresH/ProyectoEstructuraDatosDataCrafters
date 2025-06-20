3daa929af79dc0e3994176d3f23e0d7fe7696cf7
Bushhz <rafael.valdessilva@gmail.com>
2025-06-19 21:00:52 -0400

    Update Eliminar_Personas.c

--- Funciones_Eliminar/Eliminar_Personas.c
+++ Funciones_Eliminar/Eliminar_Personas.c
@@ -3,23 +3,22 @@ struct nodoPersonasABB* minimo(struct nodoPersonasABB *arbol) {
         arbol = arbol->izquierda;
     return arbol;
 }
-void eliminarPersona(struct nodoPersonasABB **personas, char *rut) {
+int eliminarPersona(struct nodoPersonasABB **personas, char *rut) {
     int comparar;
     struct nodoPersonasABB *nodoEliminar;
     struct nodoPersonasABB *minNodo;
     struct persona *aux;
 
     if (personas == NULL || *personas == NULL) {
-        printf("No se encontraron personas\n");
-        return;
+        return 0;
     }
 
     comparar = strcmp(rut, (*personas)->datosPersona->rut);
 
     if (comparar < 0) {
-        eliminarPersona(&((*personas)->izquierda), rut);
+        return eliminarPersona(&((*personas)->izquierda), rut);
     } else if (comparar > 0) {
-        eliminarPersona(&((*personas)->derecha), rut);
+        return eliminarPersona(&((*personas)->derecha), rut);
     } else {
         nodoEliminar = *personas;
 
@@ -27,19 +26,20 @@ void eliminarPersona(struct nodoPersonasABB **personas, char *rut) {
             *personas = nodoEliminar->derecha;
             liberarPersona(nodoEliminar->datosPersona);
             free(nodoEliminar);
+
         } else if (nodoEliminar->derecha == NULL) {
             *personas = nodoEliminar->izquierda;
             liberarPersona(nodoEliminar->datosPersona);
             free(nodoEliminar);
+
         } else {
             minNodo = minimo(nodoEliminar->derecha);
             aux = nodoEliminar->datosPersona;
             nodoEliminar->datosPersona = minNodo->datosPersona;
             minNodo->datosPersona = aux;
-
-            eliminarPersona(&(nodoEliminar->derecha), aux->rut);
+            return eliminarPersona(&(nodoEliminar->derecha), aux->rut);
         }
+        return 1;
     }
-
-    printf("Persona eliminada correctamente.");
+    return 0;
 }
