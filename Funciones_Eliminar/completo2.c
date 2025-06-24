9fe91dcc0bebcf48a43720428261a31d2420b45e
Bushhz <rafael.valdessilva@gmail.com>
2025-06-24 13:52:57 -0400

    Update completo2.c

--- Funciones_Eliminar/completo2.c
+++ Funciones_Eliminar/completo2.c
@@ -1,3 +1,4 @@
+
 void reenlazarCausas(struct nodoCausas **listaCausas, struct nodoCausas *nodoEliminar) {
     if (*listaCausas == NULL || nodoEliminar == NULL) return;
 
@@ -57,34 +58,46 @@ void liberarPersona(struct persona *persona) {
         return;
 
     free(persona->nombre);
+    persona->nombre = NULL;
     free(persona->apellido);
+    persona->apellido = NULL;
     free(persona->rut);
+    persona->rut = NULL;
     free(persona->contrasena);
+    persona->contrasena = NULL;
 
     free(persona);
+    persona = NULL;
 }
 void liberarDenuncia(struct denuncia *denuncia) {
     if (denuncia == NULL)
         return;
 
     free(denuncia->fecha);
+    denuncia->fecha = NULL;
     free(denuncia->descripcion);
+    denuncia->descripcion = NULL;
     free(denuncia->ruc);
+    denuncia->ruc = NULL;
     free(denuncia->tipoDeDenuncia);
-    denuncia->denunciante = NULL;
-    denuncia->denunciado = NULL;
+    denuncia->tipoDeDenuncia = NULL;
 
     free(denuncia);
+    denuncia = NULL;
 }
 void liberarDatosImputados(struct datosImputados *datos) {
     if (datos == NULL)
         return;
 
     free(datos->declaracion);
+    datos->declaracion = NULL;
     free(datos->fechaInicioMedida);
+    datos->fechaInicioMedida = NULL;
     free(datos->fechaFinMedida);
+    datos->fechaFinMedida = NULL;
 
     free(datos);
+    datos = NULL;
 }
 void liberarInvolucrados(struct involucrados **involucrados, int tam) {
     int i;
@@ -98,6 +111,7 @@ void liberarInvolucrados(struct involucrados **involucrados, int tam) {
                 liberarDatosImputados(involucrados[i]->datosImputados);
                 involucrados[i]->datosImputados = NULL;
             }
+            // No liberamos persona, asumiendo que es compartida
             free(involucrados[i]);
             involucrados[i] = NULL;
         }
@@ -110,19 +124,27 @@ void liberarDatosDiligencias(struct datosDiligencias *degligencias) {
         return;
 
     free(degligencias->fechaInicio);
+    degligencias->fechaInicio = NULL;
     free(degligencias->fechaFin);
+    degligencias->fechaFin = NULL;
     free(degligencias->descripcion);
+    degligencias->descripcion = NULL;
     free(degligencias->estado);
+    degligencias->estado = NULL;
     free(degligencias->tipoDiligencia);
+    degligencias->tipoDiligencia = NULL;
 
     free(degligencias);
+    degligencias = NULL;
 }
 void liberarDatosCarpeta(struct datosCarpeta *datosCarpeta) {
     if (datosCarpeta == NULL)
         return;
 
     free(datosCarpeta->fecha);
+    datosCarpeta->fecha = NULL;
     free(datosCarpeta->descripcion);
+    datosCarpeta->descripcion = NULL;
 
     if (datosCarpeta->datosDiligencias != NULL) {
         liberarDatosDiligencias(datosCarpeta->datosDiligencias);
@@ -130,14 +152,17 @@ void liberarDatosCarpeta(struct datosCarpeta *datosCarpeta) {
     }
 
     if (datosCarpeta->datosDenuncia != NULL) {
+        liberarDenuncia(datosCarpeta->datosDenuncia);
         datosCarpeta->datosDenuncia = NULL;
     }
 
     if (datosCarpeta->datosPersona != NULL) {
+        liberarPersona(datosCarpeta->datosPersona);
         datosCarpeta->datosPersona = NULL;
     }
 
     free(datosCarpeta);
+    datosCarpeta = NULL;
 }
 void liberarCausa(struct causa *causa) {
     struct nodoDatosCarpetas *actual;
@@ -147,9 +172,12 @@ void liberarCausa(struct causa *causa) {
         return;
 
     free(causa->ruc);
+    causa->ruc = NULL;
 
-    if (causa->sentencia != NULL)
+    if (causa->sentencia != NULL){
         free(causa->sentencia);
+        causa->sentencia = NULL;
+    }
 
     if (causa->denuncia != NULL) {
         liberarDenuncia(causa->denuncia);
@@ -178,6 +206,8 @@ void liberarCausa(struct causa *causa) {
 
     free(causa);
 }
+
+
 int eliminarCausa(struct nodoCausas **listaCausa, char *ruc) {
     struct nodoCausas *aEliminar;
 
